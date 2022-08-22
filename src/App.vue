41e94e1c5645ac499a7139428e6ea2cefc449bf2
ngodingbentar <dewaqintoro@gmail.com>
2022-08-22 23:47:54 +0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <CKEditorVue />
+  </div>
+</template>
+
+<script>
+import CKEditorVue from './components/CKEditor.vue';
+export default {
+  name: 'App',
+  components: {
+    CKEditorVue,
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
